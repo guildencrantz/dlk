3b49c694cceda74544142f21af775855859b48ba
Matt Henkel <matt.henkel@returnpath.com>
2015-09-16 13:54:05 -0600

    Move dns files.
    Change-Id: I42ec84e646feaf594cc86b7039d975cd0df5458b

--- dlk.sh
+++ dlk.sh
@@ -7,23 +7,21 @@ pushd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null
 docker-compose up -d
 
 echo -n "Waiting for API "
-  while [ 1 ]
-  do
+  while [ 1 ]; do
     sleep 1
     ! kubectl version >/dev/null || break
   done
 echo -e "\e[32mOK\e[39m"
 
 echo -n "Starting skydns  "
-  kubectl create -f kube-dns.rc.yaml >/dev/null
-  kubectl create -f kube-dns.service.yaml >/dev/null
+  kubectl create -f dns/dns.rc.yml >/dev/null
+  kubectl create -f dns/dns.service.yml >/dev/null
 echo -e "\e[32mOK\e[39m"
 
 echo -n "Verifying skydns (wait for it) "
   while [ 1 ]; do
     sleep 1
-    if nslookup google.com 10.0.0.10 >/dev/null 2>&1
-    then
+    if nslookup google.com 10.0.0.10 >/dev/null 2>&1; then
       break
     fi
   done
